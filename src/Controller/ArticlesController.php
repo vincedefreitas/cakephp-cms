00ae585dad7bbb966593280f60ed2706540b7222
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2025-05-18 07:34:05 +0100

    Create view action in ArticlesController and view.php in templates

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -11,4 +11,10 @@ class ArticlesController extends AppController
         $articles = $this->paginate($this->Articles);
         $this->set(compact('articles'));
     }
+
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
 }
